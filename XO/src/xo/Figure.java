c8a9409a084900c54e71582fa730eef73bb1896d
Yura Gorelik <33177008+Yura114345@users.noreply.github.com>
2018-03-28 21:10:29 +0300

    Add class Board, class Figure, class Main

---
+++ XO/src/xo/Figure.java
@@ -0,0 +1,11 @@
+package xo;
+
+public class Figure {
+	
+	private final String figure;
+	
+	public Figure(final String figure) { this.figure = figure; }
+
+	public String getFigure() { return figure; }
+
+}
---
+++ XO/src/xo/Main.java
@@ -0,0 +1,11 @@
+package xo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final Board board = new Board();
+		Board.printGameName();
+		board.printBoard();
+	}
+	
+}
